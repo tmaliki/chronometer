b9f06f1b7c171585d8b8204c6dacdd8a59db086e
tmaliki <maliki.tcherou1@gmail.com>
2025-04-25 12:12:09 +0100

    updated 20250425 1212

--- assets/chrono.js
+++ assets/chrono.js
@@ -3,10 +3,10 @@
  */
 let timerId = 0;
 
-let chronoId = document.querySelector('#chronoId');
-let startId = document.querySelector('#startId');
-let stopId = document.querySelector('#stopId');
-let resetId = document.querySelector('#resetId');
+let chronoId = document.querySelector("#chronoId");
+let startId = document.querySelector("#startId");
+let stopId = document.querySelector("#stopId");
+let resetId = document.querySelector("#resetId");
 
 let heure = 0;
 let minutes = 0;
@@ -14,57 +14,57 @@ let secondes = 0;
 let msecondes = 0;
 
 function numberFormat(n) {
-    return n.toString().padStart(2, '0');
+  return n.toString().padStart(2, "0");
 }
 
 // start chrono
-startId.addEventListener('click', startChrono);
+startId.addEventListener("click", startChrono);
 function startChrono() {
-    timerId = setInterval(() => {
-        // affichage
-        // let heureShow = (heure < 10) ? "0"+ heure : heure;
-        // let minutesShow = (minutes < 10) ? "0"+ minutes : minutes;
-        // let secondesShow = (secondes < 10) ? "0"+ secondes : secondes;
-        // let msecondesShow = (msecondes < 10) ? "0"+ msecondes : msecondes;
-        // chronoId.innerHTML = heureShow +" : "+ minutesShow +" : "+ secondesShow +" : "+ msecondesShow;
+  timerId = setInterval(() => {
+    // affichage
+    // let heureShow = (heure < 10) ? "0"+ heure : heure;
+    // let minutesShow = (minutes < 10) ? "0"+ minutes : minutes;
+    // let secondesShow = (secondes < 10) ? "0"+ secondes : secondes;
+    // let msecondesShow = (msecondes < 10) ? "0"+ msecondes : msecondes;
+    // chronoId.innerHTML = heureShow +" : "+ minutesShow +" : "+ secondesShow +" : "+ msecondesShow;
 
-        chronoId.innerHTML = numberFormat(heure) +" : "+ numberFormat(minutes) +" : "+ numberFormat(secondes) +" : "+ msecondes;
-        
-        msecondes += 1;
-        if(msecondes >= 10) {
-            msecondes = 0;
-            secondes += 1;
-        }
+    chronoId.innerHTML = numberFormat(heure) + " : " + numberFormat(minutes) + " : " + numberFormat(secondes) + " : " + msecondes;
 
-        if(secondes >= 60) {
-            secondes = 0;
-            minutes += 1;
-        }
-        
-        if(minutes >= 60) {
-            minutes = 0;
-            heure += 1;
-        }
-    }, 100);
+    msecondes += 1;
+    if (msecondes >= 100) {
+      msecondes = 0;
+      secondes += 1;
+    }
 
-    startId.setAttribute('disabled', '');
+    if (secondes >= 60) {
+      secondes = 0;
+      minutes += 1;
+    }
+
+    if (minutes >= 60) {
+      minutes = 0;
+      heure += 1;
+    }
+  }, 10);
+
+  startId.setAttribute("disabled", "");
 }
 
 // stop chrono
-stopId.addEventListener('click', stopChrono);
+stopId.addEventListener("click", stopChrono);
 function stopChrono() {
-    clearInterval(timerId);
-    startId.removeAttribute('disabled');
+  clearInterval(timerId);
+  startId.removeAttribute("disabled");
 }
 
 // reset chrono
-resetId.addEventListener('click', resetChrono);
+resetId.addEventListener("click", resetChrono);
 function resetChrono() {
-    heure = 0;
-    minutes = 0;
-    secondes = 0;
-    msecondes = 0;
-    clearInterval(timerId);
-    chronoId.innerHTML = "00 : 00 : 00 : 00";
-    startId.removeAttribute('disabled');
+  heure = 0;
+  minutes = 0;
+  secondes = 0;
+  msecondes = 0;
+  clearInterval(timerId);
+  chronoId.innerHTML = "00 : 00 : 00 : 00";
+  startId.removeAttribute("disabled");
 }
